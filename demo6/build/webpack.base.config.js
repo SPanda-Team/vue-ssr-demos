a736b65fe6dec40b287a3a849550652d52e302b8
Sol0423 <978481755@qq.com>
2021-01-19 14:54:43 +0800

    兼容es6

--- demo6/build/webpack.base.config.js
+++ demo6/build/webpack.base.config.js
@@ -33,10 +33,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/,
-        options:{
-          "presets": ["@babel/preset-env"]
-        }
+        exclude: /node_modules/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
